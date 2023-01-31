cd6fc9bae40d68261775d967d56e617c3f0ee330
Boniiiiie <88722429+yeon93@users.noreply.github.com>
2023-01-31 18:03:09 +0900

    Add files via upload

---
+++ HelloCSharpWin/Calculator.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace HelloCSharpWin
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumButton1 = new System.Windows.Forms.Button();
+            this.NumButton2 = new System.Windows.Forms.Button();
+            this.NumButton3 = new System.Windows.Forms.Button();
+            this.NumButton4 = new System.Windows.Forms.Button();
+            this.NumButton5 = new System.Windows.Forms.Button();
+            this.NumButton6 = new System.Windows.Forms.Button();
+            this.NumButton7 = new System.Windows.Forms.Button();
+            this.NumButton8 = new System.Windows.Forms.Button();
+            this.NumButton9 = new System.Windows.Forms.Button();
+            this.ResultScreen = new System.Windows.Forms.Label();
+            this.NumButton0 = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SubButton = new System.Windows.Forms.Button();
+            this.MulButton = new System.Windows.Forms.Button();
+            this.DivButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.ProcessScreen = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DecimalPoint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumButton1
+            // 
+            this.NumButton1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton1.Location = new System.Drawing.Point(29, 380);
+            this.NumButton1.Name = "NumButton1";
+            this.NumButton1.Size = new System.Drawing.Size(81, 63);
+            this.NumButton1.TabIndex = 1;
+            this.NumButton1.Text = "1";
+            this.NumButton1.UseVisualStyleBackColor = true;
+            this.NumButton1.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton2
+            // 
+            this.NumButton2.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton2.Location = new System.Drawing.Point(120, 380);
+            this.NumButton2.Name = "NumButton2";
+            this.NumButton2.Size = new System.Drawing.Size(81, 63);
+            this.NumButton2.TabIndex = 2;
+            this.NumButton2.Text = "2";
+            this.NumButton2.UseVisualStyleBackColor = true;
+            this.NumButton2.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton3
+            // 
+            this.NumButton3.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton3.Location = new System.Drawing.Point(209, 380);
+            this.NumButton3.Name = "NumButton3";
+            this.NumButton3.Size = new System.Drawing.Size(81, 63);
+            this.NumButton3.TabIndex = 3;
+            this.NumButton3.Text = "3";
+            this.NumButton3.UseVisualStyleBackColor = true;
+            this.NumButton3.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton4
+            // 
+            this.NumButton4.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton4.Location = new System.Drawing.Point(28, 311);
+            this.NumButton4.Name = "NumButton4";
+            this.NumButton4.Size = new System.Drawing.Size(81, 63);
+            this.NumButton4.TabIndex = 4;
+            this.NumButton4.Text = "4";
+            this.NumButton4.UseVisualStyleBackColor = true;
+            this.NumButton4.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton5
+            // 
+            this.NumButton5.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton5.Location = new System.Drawing.Point(118, 311);
+            this.NumButton5.Name = "NumButton5";
+            this.NumButton5.Size = new System.Drawing.Size(81, 63);
+            this.NumButton5.TabIndex = 5;
+            this.NumButton5.Text = "5";
+            this.NumButton5.UseVisualStyleBackColor = true;
+            this.NumButton5.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton6
+            // 
+            this.NumButton6.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton6.Location = new System.Drawing.Point(207, 311);
+            this.NumButton6.Name = "NumButton6";
+            this.NumButton6.Size = new System.Drawing.Size(81, 63);
+            this.NumButton6.TabIndex = 6;
+            this.NumButton6.Text = "6";
+            this.NumButton6.UseVisualStyleBackColor = true;
+            this.NumButton6.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton7
+            // 
+            this.NumButton7.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton7.Location = new System.Drawing.Point(28, 242);
+            this.NumButton7.Name = "NumButton7";
+            this.NumButton7.Size = new System.Drawing.Size(81, 63);
+            this.NumButton7.TabIndex = 7;
+            this.NumButton7.Text = "7";
+            this.NumButton7.UseVisualStyleBackColor = true;
+            this.NumButton7.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton8
+            // 
+            this.NumButton8.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton8.Location = new System.Drawing.Point(120, 242);
+            this.NumButton8.Name = "NumButton8";
+            this.NumButton8.Size = new System.Drawing.Size(81, 63);
+            this.NumButton8.TabIndex = 8;
+            this.NumButton8.Text = "8";
+            this.NumButton8.UseVisualStyleBackColor = true;
+            this.NumButton8.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton9
+            // 
+            this.NumButton9.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton9.Location = new System.Drawing.Point(207, 242);
+            this.NumButton9.Name = "NumButton9";
+            this.NumButton9.Size = new System.Drawing.Size(81, 63);
+            this.NumButton9.TabIndex = 9;
+            this.NumButton9.Text = "9";
+            this.NumButton9.UseVisualStyleBackColor = true;
+            this.NumButton9.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // ResultScreen
+            // 
+            this.ResultScreen.BackColor = System.Drawing.Color.White;
+            this.ResultScreen.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ResultScreen.Location = new System.Drawing.Point(30, 35);
+            this.ResultScreen.Name = "ResultScreen";
+            this.ResultScreen.Size = new System.Drawing.Size(346, 70);
+            this.ResultScreen.TabIndex = 10;
+            this.ResultScreen.Text = "0";
+            this.ResultScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // NumButton0
+            // 
+            this.NumButton0.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumButton0.Location = new System.Drawing.Point(122, 449);
+            this.NumButton0.Name = "NumButton0";
+            this.NumButton0.Size = new System.Drawing.Size(79, 63);
+            this.NumButton0.TabIndex = 11;
+            this.NumButton0.Text = "0";
+            this.NumButton0.UseVisualStyleBackColor = true;
+            this.NumButton0.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // Equal
+            // 
+            this.Equal.BackColor = System.Drawing.Color.SlateGray;
+            this.Equal.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Equal.Location = new System.Drawing.Point(295, 449);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(79, 63);
+            this.Equal.TabIndex = 15;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = false;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackColor = System.Drawing.Color.Transparent;
+            this.AddButton.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.AddButton.Location = new System.Drawing.Point(296, 380);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(81, 63);
+            this.AddButton.TabIndex = 14;
+            this.AddButton.Text = "+";
+            this.AddButton.UseVisualStyleBackColor = false;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // SubButton
+            // 
+            this.SubButton.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.SubButton.Location = new System.Drawing.Point(296, 311);
+            this.SubButton.Name = "SubButton";
+            this.SubButton.Size = new System.Drawing.Size(81, 63);
+            this.SubButton.TabIndex = 13;
+            this.SubButton.Text = "-";
+            this.SubButton.UseVisualStyleBackColor = true;
+            this.SubButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // MulButton
+            // 
+            this.MulButton.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MulButton.Location = new System.Drawing.Point(296, 242);
+            this.MulButton.Name = "MulButton";
+            this.MulButton.Size = new System.Drawing.Size(81, 63);
+            this.MulButton.TabIndex = 12;
+            this.MulButton.Text = "x";
+            this.MulButton.UseVisualStyleBackColor = true;
+            this.MulButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // DivButton
+            // 
+            this.DivButton.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DivButton.Location = new System.Drawing.Point(296, 173);
+            this.DivButton.Name = "DivButton";
+            this.DivButton.Size = new System.Drawing.Size(81, 63);
+            this.DivButton.TabIndex = 16;
+            this.DivButton.Text = "÷";
+            this.DivButton.UseVisualStyleBackColor = true;
+            this.DivButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ClearButton.Location = new System.Drawing.Point(207, 173);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(80, 63);
+            this.ClearButton.TabIndex = 17;
+            this.ClearButton.Text = "C";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ProcessScreen
+            // 
+            this.ProcessScreen.Font = new System.Drawing.Font("맑은 고딕", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ProcessScreen.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ProcessScreen.Location = new System.Drawing.Point(28, 19);
+            this.ProcessScreen.Name = "ProcessScreen";
+            this.ProcessScreen.Size = new System.Drawing.Size(349, 29);
+            this.ProcessScreen.TabIndex = 18;
+            this.ProcessScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(120, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 63);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "CE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // DecimalPoint
+            // 
+            this.DecimalPoint.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DecimalPoint.Location = new System.Drawing.Point(209, 449);
+            this.DecimalPoint.Name = "DecimalPoint";
+            this.DecimalPoint.Size = new System.Drawing.Size(75, 63);
+            this.DecimalPoint.TabIndex = 21;
+            this.DecimalPoint.Text = ".";
+            this.DecimalPoint.UseVisualStyleBackColor = true;
+            this.DecimalPoint.Click += new System.EventHandler(this.DecimalPoint_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(401, 565);
+            this.Controls.Add(this.DecimalPoint);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ProcessScreen);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.DivButton);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SubButton);
+            this.Controls.Add(this.MulButton);
+            this.Controls.Add(this.NumButton0);
+            this.Controls.Add(this.ResultScreen);
+            this.Controls.Add(this.NumButton9);
+            this.Controls.Add(this.NumButton8);
+            this.Controls.Add(this.NumButton7);
+            this.Controls.Add(this.NumButton6);
+            this.Controls.Add(this.NumButton5);
+            this.Controls.Add(this.NumButton4);
+            this.Controls.Add(this.NumButton3);
+            this.Controls.Add(this.NumButton2);
+            this.Controls.Add(this.NumButton1);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button NumButton1;
+        private System.Windows.Forms.Button NumButton2;
+        private System.Windows.Forms.Button NumButton3;
+        private System.Windows.Forms.Button NumButton4;
+        private System.Windows.Forms.Button NumButton5;
+        private System.Windows.Forms.Button NumButton6;
+        private System.Windows.Forms.Button NumButton7;
+        private System.Windows.Forms.Button NumButton8;
+        private System.Windows.Forms.Button NumButton9;
+        private System.Windows.Forms.Label ResultScreen;
+        private System.Windows.Forms.Button NumButton0;
+        private System.Windows.Forms.Button Equal;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button SubButton;
+        private System.Windows.Forms.Button MulButton;
+        private System.Windows.Forms.Button DivButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Label ProcessScreen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button DecimalPoint;
+    }
+}
+
---
+++ HelloCSharpWin/Calculator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calculator : Form
+    {
+        public double Result = 0;
+        public string TempNum = "";     //연산 버튼을 누를 때까지 입력받은 숫자와 소수점 저장 => double.Parse() 통해 숫자로 변환
+        public string InputNum = "";
+        public string calProcessing = "";
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        //더하기
+        public double Add(double number1, double number2)
+        {
+            double result = number1 + number2;
+            return result;
+        }
+        //빼기
+        public double Subtract(double number1, double number2)
+        {
+            double result = number1 - number2;
+            return result;
+        }
+        //곱하기
+        public double Multiplicate(double number1, double number2)
+        {
+            double result = number1 * number2;
+            return result;
+        }
+        //나누기
+        public double Divide(double number1, double number2)
+        {
+            double result = number1 / number2;
+            return result;
+        }
+
+
+        //숫자버튼
+        public void SetNum(string num)
+        {
+            if (InputNum == "")
+            {
+                ResultScreen.Text = num;
+            }
+            else if (ResultScreen.Text == "0")
+            { ResultScreen.Text = num; }
+            else
+            { ResultScreen.Text = ResultScreen.Text + num; }
+            InputNum = InputNum + num;
+        }
+
+
+        private void NumButton1_Click(object sender, EventArgs e)
+        {
+            Button numButton = (Button)sender;
+            SetNum(numButton.Text);
+            InputNum = InputNum + numButton.Text;
+            calProcessing = calProcessing + numButton.Text;
+        }
+
+        //사칙연산 버튼
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (InputNum == "") { }  //숫자 입력 없는데 사칙연산 버튼 누르면 pass
+            else
+            {
+                calProcessing = calProcessing + " + ";
+                TempNum = InputNum;
+                ProcessScreen.Text = calProcessing;
+                Result = double.Parse(InputNum);
+                
+            }
+            InputNum = "";
+        }
+
+
+        private void Equal_Click(object sender, EventArgs e)
+        {
+            ResultScreen.Text = Result.ToString();
+            ProcessScreen.Text = calProcessing + " =";
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Result = 0;
+
+            ResultScreen.Text = "0";
+            ProcessScreen.Text = "";
+            calProcessing = "";
+        }
+
+
+        private void DecimalPoint_Click(object sender, EventArgs e)  //소수점
+        {
+            //ResultScreen.Text 가 int일 때 => int + "."
+            //이미 .이 있을 때 => pass
+            //InputNum = InputNum + "."
+        }
+    }
+}
